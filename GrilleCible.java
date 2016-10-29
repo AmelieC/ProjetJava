2baa0ff6cadeaa4c4facb9837c3a8fdf4841b50b
superuserC <he201447@students.ephec.be>
2016-10-30 00:21:32 +0200

    Create GrilleCible.java

---
+++ GrilleCible.java
@@ -0,0 +1,13 @@
+package projet;
+/**
+ * Cette classe détermine la grille avec laquelle le joueur prendra ses décisions.
+ * @author kevin
+ *
+ */
+public class GrilleCible extends GrilleJeu{
+	
+	public GrilleCible(int nbColonnes,int nbLignes){
+		super(nbColonnes,nbLignes);
+	}
+
+}
