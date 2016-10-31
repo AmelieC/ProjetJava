016acac00b038c12b7c0e42d5a7b5910be898696
superuserC <he201447@students.ephec.be>
2016-10-31 13:03:31 +0100

    Création nouvelle classes pour méthodes spéciales
    Les méthodes de création de la grille en mode console sont déplacées dans la classe GrilleMaker pour plus de lisibilité dans la classe main.

--- Main.java
+++ Main.java
@@ -16,73 +16,15 @@ public class Main {
 		nbColonnes=Integer.parseInt(args[0])+1;
 		nbLignes=Integer.parseInt(args[1])+1;
 		Joueur moi=new Joueur("kevin","00000001");
-		Main.afficherGrillePerso(moi);
+		GrilleMaker.afficherGrillePerso(moi);
 		moi.getGrillePropre().getTabCase()[10][10].setEstUtilisee(true);
 		moi.getGrillePropre().getTabCase()[10][11].setEstUtilisee(true);
 		moi.getGrillePropre().getTabCase()[10][12].setEstUtilisee(true);
 		moi.getGrillePropre().getTabCase()[10][13].setEstUtilisee(true);
 		moi.getGrillePropre().getTabCase()[10][14].setEstUtilisee(true);
-		Main.afficherGrillePerso(moi);
+		GrilleMaker.afficherGrillePerso(moi);
 		moi.getGrillePropre().getTabCase()[10][10].setEstDetruite(true);
-		Main.afficherGrillePerso(moi);
+		GrilleMaker.afficherGrillePerso(moi);
 	
 	}
-	/**
-	 * Cette méthode se charge de l'affichage de la grille du programme en mode console !
-	 * Si une case non détruite ne possède aucun navire, on affichera #
-	 * Si une case possède un navire, on affichera @
-	 * Si une case détruie possède un navire, on affichera D
-	 * Si une case détruite ne possède pas de navire, on affichera X
-	 * @param joueur
-	 */
-	public static void afficherGrillePerso(Joueur joueur){
-		for(int i=0;i<Main.nbLignes;i++){
-			for(int j=0;j<Main.nbColonnes;j++){
-				if(i==0){//première ligne
-					if(j==0){
-						System.out.print("   ");
-					}
-					else{
-						if(j<10){
-							System.out.print(j+"  ");
-						}
-						else{
-							System.out.print(j+" ");
-						}
-					}	
-				}
-				else{
-					if(j==0){
-						if(i<=26){	
-							System.out.print(alphabet[i-1]+"  ");
-						}
-						else{
-							int indice=(int) (i-0.1)/26;//petite triche pour avoir AZ puis BA et non BZ puis BA
-							System.out.print(alphabet[indice-1]);
-							System.out.print(alphabet[(i-1)%26]+" ");
-						}
-					}
-					else{
-						if(!joueur.getGrillePropre().getTabCase()[i][j].isEstUtilisee()){
-							System.out.print("#  ");
-						}
-						else if(joueur.getGrillePropre().getTabCase()[i][j].isEstUtilisee() && joueur.getGrillePropre().getTabCase()[i][j].isEstDetruite()){
-							System.out.print("D  ");
-						}
-						else if(joueur.getGrillePropre().getTabCase()[i][j].isEstUtilisee()){
-							System.out.print("@  ");
-						}
-						else if(!joueur.getGrillePropre().getTabCase()[i][j].isEstUtilisee() && joueur.getGrillePropre().getTabCase()[i][j].isEstDetruite()){
-							System.out.print("X  ");
-						}
-					}	
-				}
-			}
-			System.out.print("\n");
-		}
-	}
-	public static void affichierGrilleCible(Joueur joueur){
-		
-	}
 }
-
