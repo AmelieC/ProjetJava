d3b6780da6a28d6ebac7e78e68d2757b0be9e06a
superuserC <he201447@students.ephec.be>
2016-12-06 21:04:40 +0100

    Update BatailleController.java

--- BatailleController.java
+++ BatailleController.java
@@ -10,40 +10,60 @@ import view.BatailleVue;
 public class BatailleController {
 
 	private GrilleJeuModel	grilleJeuModel;
+	private GrilleJeuModel grilleCible;
 	private JoueurModel joueurModel;
-	
-	private GrilleJeuModel	grilleJeuModelB;
-	private JoueurModel joueurModelB;
 	
 	private BatailleVue view = null;
 
-	public BatailleController(GrilleJeuModel grilleJeuModel,JoueurModel joueurModel,GrilleJeuModel grilleJeuModeB,JoueurModel joueurModelB){
+	public BatailleController(GrilleJeuModel grilleJeuModel,JoueurModel joueurModel,GrilleJeuModel grilleCible){
 		
 		this.grilleJeuModel = grilleJeuModel;
 		this.joueurModel = joueurModel;
-		this.grilleJeuModelB = grilleJeuModelB;
-		this.joueurModelB = joueurModelB;
+		this.grilleCible = grilleCible;
 	}
 	/**
 	 * Cette méthode positionne les navires du joueur lorsque celui-ci entre des coordonées
 	 * @param valeur
 	 */
-	public void positionner(String valeur,int nbBateau,int indiceBateau,JoueurModel joueurModel){
+	public boolean positionner(String valeur,int indiceBateau,int nbBateau){
 		int [] resultat;
 		resultat = decodeur(valeur);
 		if(!verifPosition(resultat)){
 			System.out.println("Case deja occupee");
+			return false;
 		}
+		
 		else{
-			joueurModel.getGrilleJeu().setEstUtilisee(true,resultat[0], resultat[1]);;
+			joueurModel.getGrilleJeu().setEstUtilisee(true,resultat[0], resultat[1]);
+			joueurModel.getGrilleJeu().getTabCase()[resultat[0]][resultat[0]].setBateau(joueurModel.getBateau()[nbBateau]);
 			//afficherGrillePerso();
 			joueurModel.getBateau()[nbBateau].setPositionToIndex(joueurModel.getGrilleJeu().getTabCase()[resultat[0]][resultat[1]], indiceBateau);
-			
+			return true;
 			//if(joueurModel.getBateau()[nbBateau].getTaille()==indiceBateau)
 		}
-		
-		
 	}
+	/**
+	 * Cette methode controle si l'entree de l'utilisateur adverse touche un bateau ou non.
+	 * Elle met à jour les modeles en conséquence.
+	 * @param valeurs
+	 * @return TOUCHE si la case était occupée par un navire. RATE sinon.
+	 */
+	public String verifierDegats(int [] valeurs){
+		if(joueurModel.getGrilleJeu().getTabCase()[valeurs[0]][valeurs[1]].isEstUtilisee()){
+			joueurModel.getGrilleJeu().getTabCase()[valeurs[0]][valeurs[1]].setEstDetruite(true);
+			return "TOUCHE";
+			
+		}
+		else{
+			joueurModel.getGrilleJeu().getTabCase()[valeurs[0]][valeurs[1]].setEstDetruite(true);
+			return "RATE";
+		}
+	}
+	
+	public void addView(BatailleVue view){
+		this.view=view;
+	}
+	
 	/**
 	 * Cette méthode vérifie qu'une case n'est pas déjà occupée par un navire
 	 * Retourne true si la case est vide, false sinon.
@@ -51,9 +71,6 @@ public class BatailleController {
 	 * @param joueur
 	 * @return
 	 */
-	public void addView(BatailleVue view){
-		this.view=view;
-	}
 	public boolean verifPosition(int [] position){
 		int i=1;
 		int j=1;
@@ -70,19 +87,22 @@ public class BatailleController {
 		}
 		return true;
 	}
-	public void tirer(String valeur){ //a modifier pour une utilisation a 2 joueur
+	public void updateGrilleCible(String rep,String msg){
 		int [] resultat;
-		resultat = decodeur(valeur);
-		if(joueurModelB.getGrilleJeu().getTabCase()[resultat[0]][resultat[1]].isEstUtilisee()){
-			System.out.println("TOUCHE");
-			joueurModelB.getGrilleJeu().setEstDetruite(true, resultat[0], resultat[1]);
+		resultat = decodeur(msg);
+		if(rep.equals("TOUCHE")){ //veut dire que l'on a touche !
+			joueurModel.getGrilleCible().getTabCase()[resultat[0]][resultat[1]].setEstUtilisee(true);
+			joueurModel.getGrilleCible().getTabCase()[resultat[0]][resultat[1]].setEstDetruite(true);
 		}
 		else{
-			System.out.println("X");
-			joueurModelB.getGrilleJeu().setEstDetruite(true, resultat[0], resultat[1]);
+			joueurModel.getGrilleCible().getTabCase()[resultat[0]][resultat[1]].setEstUtilisee(false);
+			joueurModel.getGrilleCible().getTabCase()[resultat[0]][resultat[1]].setEstDetruite(true);
 		}
-		
 	}
+	
+	
+	
+	
 	/**
 	 * Transforme une entrée utilisateur (par exemple A-5) en indice pour le tableau à 2 dimensions
 	 * Distinction entre (A-8) et (BC-4) par exemple.
@@ -91,6 +111,7 @@ public class BatailleController {
 	public int[] decodeur(String entree){
 		String tab[]=new String [2];
 		int Resultat[]= new int[2];
+		
 		tab=entree.split("-");
 		Resultat[1]=Integer.parseInt(tab[1]);
 		if(tab[0].length()==1){//exemple (A-10)
@@ -119,7 +140,7 @@ public class BatailleController {
 		return Resultat;
 		
 	}
-	public void afficherGrillePerso(JoueurModel joueurModel){
+	public void afficherGrillePerso(){
 		for(int i=0;i<Main.nbLignes;i++){
 			for(int j=0;j<Main.nbColonnes;j++){
 				if(i==0){//première ligne
@@ -147,7 +168,7 @@ public class BatailleController {
 						}
 					}
 					else{
-						if(!joueurModel.getGrilleJeu().getTabCase()[i][j].isEstUtilisee()){
+						if(!joueurModel.getGrilleJeu().getTabCase()[i][j].isEstUtilisee() && !joueurModel.getGrilleJeu().getTabCase()[i][j].isEstDetruite()){
 							System.out.print("~  ");
 						}
 						else if(joueurModel.getGrilleJeu().getTabCase()[i][j].isEstUtilisee() && joueurModel.getGrilleJeu().getTabCase()[i][j].isEstDetruite()){
@@ -173,7 +194,7 @@ public class BatailleController {
  * Si une case détruite ne possède pas de navire, on affichera X
  * @param joueur
  */
-public void afficherGrilleCible(JoueurModel joueurModel){
+public void afficherGrilleCible(){
 	for(int i=0;i<Main.nbLignes;i++){
 		for(int j=0;j<Main.nbColonnes;j++){
 			if(i==0){//première ligne
@@ -201,16 +222,16 @@ public void afficherGrilleCible(JoueurModel joueurModel){
 					}
 				}
 				else{
-					if(!joueurModel.getGrilleJeu().getTabCase()[i][j].isEstUtilisee() && !joueurModel.getGrilleJeu().getTabCase()[i][j].isEstDetruite()){
+					if(!joueurModel.getGrilleCible().getTabCase()[i][j].isEstUtilisee() && !joueurModel.getGrilleCible().getTabCase()[i][j].isEstDetruite()){
 						System.out.print("~  ");
 					}
-					else if(joueurModel.getGrilleJeu().getTabCase()[i][j].isEstUtilisee() && joueurModel.getGrilleJeu().getTabCase()[i][j].isEstDetruite()){
+					else if(joueurModel.getGrilleCible().getTabCase()[i][j].isEstUtilisee() && joueurModel.getGrilleCible().getTabCase()[i][j].isEstDetruite()){
 						System.out.print("T  ");
 					}
-					else if(joueurModel.getGrilleJeu().getTabCase()[i][j].isEstUtilisee()){
+					else if(joueurModel.getGrilleCible().getTabCase()[i][j].isEstUtilisee()){
 						System.out.print("~  "); //Le joueur ne peut pas voir la posision de navires ennemis 
 					}
-					else if(!joueurModel.getGrilleJeu().getTabCase()[i][j].isEstUtilisee() && joueurModel.getGrilleJeu().getTabCase()[i][j].isEstDetruite()){
+					else if(!joueurModel.getGrilleCible().getTabCase()[i][j].isEstUtilisee() && joueurModel.getGrilleCible().getTabCase()[i][j].isEstDetruite()){
 						System.out.print("X  ");
 					}
 				}	
@@ -238,20 +259,24 @@ public static void positionOrdinateur(JoueurModel joueur){
 				if(vertOrHori<0.5){ //on positionne verticalement
 					if(caseInitY<joueur.getBateau()[i].getTaille()){ //positionnement descendant
 						joueur.getGrilleJeu().getTabCase()[caseInitX][caseInitY+j].setEstUtilisee(true);
+						joueur.getGrilleJeu().getTabCase()[caseInitX][caseInitY+j].setBateau(joueur.getBateau()[i]);
 						joueur.getBateau()[i].setPositionToIndex(joueur.getGrilleJeu().getTabCase()[caseInitX][caseInitY+j], j);
 					}
 					else{//positionnement montant
 						joueur.getGrilleJeu().getTabCase()[caseInitX][caseInitY-j].setEstUtilisee(true);
+						joueur.getGrilleJeu().getTabCase()[caseInitX][caseInitY+j].setBateau(joueur.getBateau()[i]);
 						joueur.getBateau()[i].setPositionToIndex(joueur.getGrilleJeu().getTabCase()[caseInitX][caseInitY-j], j);
 					}
 				}
 				else{ //on position horizontalement
 					if(caseInitX<joueur.getBateau()[i].getTaille()){ //positionnement vers la droite
 						joueur.getGrilleJeu().getTabCase()[caseInitX+j][caseInitY].setEstUtilisee(true);
+						joueur.getGrilleJeu().getTabCase()[caseInitX][caseInitY+j].setBateau(joueur.getBateau()[i]);
 						joueur.getBateau()[i].setPositionToIndex(joueur.getGrilleJeu().getTabCase()[caseInitX+j][caseInitY], j);
 					}
 					else{//positionnement vers la gauche
 						joueur.getGrilleJeu().getTabCase()[caseInitX-j][caseInitY].setEstUtilisee(true);
+						joueur.getGrilleJeu().getTabCase()[caseInitX][caseInitY+j].setBateau(joueur.getBateau()[i]);
 						joueur.getBateau()[i].setPositionToIndex(joueur.getGrilleJeu().getTabCase()[caseInitX-j][caseInitY], j);
 					}
 				}
@@ -264,3 +289,4 @@ public static void positionOrdinateur(JoueurModel joueur){
 }
 	
 }
+
