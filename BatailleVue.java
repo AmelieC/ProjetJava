63b3eda630939bd3158db2e81bcef252f4bd932e
superuserC <he201447@students.ephec.be>
2016-12-22 22:03:13 +0100

    Update BatailleVue.java

--- BatailleVue.java
+++ BatailleVue.java
@@ -2,31 +2,33 @@ package view;
 
 import java.util.Observer;
 
+import javax.swing.JFrame;
+
 import controller.BatailleController;
-import default_package.Main;
-import model.BateauModel;
-import model.CaseModel;
 import model.GrilleJeuModel;
 import model.JoueurModel;
 
-public abstract class BatailleVue implements Observer{
+public abstract class BatailleVue extends JFrame implements Observer{
 	
 	protected GrilleJeuModel grilleJeuModel;
 	protected JoueurModel joueurModel;
+	protected GrilleJeuModel grilleCible;
 	
 	protected BatailleController controller;
 	
-	public BatailleVue(GrilleJeuModel grilleJeuModel,JoueurModel joueurModel,BatailleController controller){
+	public BatailleVue(GrilleJeuModel grilleJeuModel,JoueurModel joueurModel,GrilleJeuModel grilleCible,BatailleController controller){
 		
 		
 		this.grilleJeuModel = grilleJeuModel;
 		this.joueurModel = joueurModel;
+		this.grilleCible = grilleCible;
 		this.controller = controller;
+		
 		
 		//on précise que cette classe observe les models
 		joueurModel.addObserver(this);
 		grilleJeuModel.addObserver(this);
-		
+		grilleCible.addObserver(this);
 	}
 
 	
